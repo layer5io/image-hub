d705bc4044dce3a97a3b56a094d4cf554b97d11d
kanishkarj <kanishkarj@hotmail.com>
2020-05-17 13:08:34 +0530

    Ratelimiting specific to user
    Signed-off-by: kanishkarj <kanishkarj@hotmail.com>

--- rate-limit-filter/src/lib.rs
+++ rate-limit-filter/src/lib.rs
@@ -6,8 +6,6 @@ use std::time::Duration;
 use rate_limiter::RateLimiter;
 use std::time::SystemTime;
 
-// static mut rl: RateLimiter = RateLimiter::new();
-
 #[no_mangle]
 pub fn _start() {
     proxy_wasm::set_log_level(LogLevel::Info);
@@ -31,27 +29,33 @@ impl UpstreamCall {
 
 impl HttpContext for UpstreamCall {
     fn on_http_request_headers(&mut self, _num_headers: usize) -> Action {
-        let curr = self.get_current_time();
-        let tm = curr.duration_since(SystemTime::UNIX_EPOCH).unwrap();
-        let mn = (tm.as_secs()/60)%60;
-        let sc = tm.as_secs()%60;
-        // unsafe {
-            // }
-            let mut rl = RateLimiter::get();
-                    if !rl.update(mn as i32) {
-                        self.send_http_response(
-                            429,
-                            vec![("Powered-By", "proxy-wasm")],
-                            Some(b"Limit exceeded.\n"),
-                        );
-                    rl.set();
-                    return Action::Pause
-                    }
-                    proxy_wasm::hostcalls::log(LogLevel::Debug, format!("Obj {:?}", &rl).as_str());
-                    
-                    rl.set();
-
-        Action::Continue
+        if let Some(header) = self.get_http_request_header("Authorization") {
+            let curr = self.get_current_time();
+            let tm = curr.duration_since(SystemTime::UNIX_EPOCH).unwrap();
+            let mn = (tm.as_secs()/60)%60;
+            let sc = tm.as_secs()%60;
+            let mut rl = RateLimiter::get(header);
+                        if !rl.update(mn as i32) {
+                            self.send_http_response(
+                                429,
+                                vec![("Powered-By", "proxy-wasm")],
+                                Some(b"Limit exceeded.\n"),
+                            );
+                        rl.set();
+                        return Action::Pause
+                        }
+                        proxy_wasm::hostcalls::log(LogLevel::Debug, format!("Obj {:?}", &rl).as_str());
+                        
+                        rl.set();
+    
+            return Action::Continue
+        }
+        self.send_http_response(
+            401,
+            vec![("Powered-By", "proxy-wasm")],
+            Some(b"Unauthorized\n"),
+        );
+        Action::Pause
     }
     
     
--- rate-limit-filter/src/rate_limiter.rs
+++ rate-limit-filter/src/rate_limiter.rs
@@ -3,23 +3,25 @@ use proxy_wasm;
 
 const LT: u32 = 10;
 
-#[derive(Debug,Serialize,Deserialize,Copy,Clone)]
+#[derive(Debug,Serialize,Deserialize,Clone)]
 pub struct RateLimiter {
     RPM: u32,
     Min: i32,
-    pub Count: u32, 
+    Count: u32,
+    key: String,
 }
 
 impl RateLimiter {
-    pub const fn new() -> Self {
+    fn new(key: &String) -> Self {
         Self {
             RPM: LT,
             Min: -1,
             Count: 0,
+            key: key.clone()
         }
     }
-    pub fn get() -> Self {
-        if let Ok(data) = proxy_wasm::hostcalls::get_shared_data("key") {
+    pub fn get(key: String) -> Self {
+        if let Ok(data) = proxy_wasm::hostcalls::get_shared_data(&key.clone()) {
             if let Some(data) = data.0 {
                 let data: Option<Self> = bincode::deserialize(&data).unwrap_or(None);
                 if let Some(obj) = data {
@@ -27,12 +29,12 @@ impl RateLimiter {
                 }
             }
         }
-        return Self::new()
+        return Self::new(&key)
     }
     pub fn set(&self) {
         let target: Option<Self>  = Some(self.clone());
         let encoded: Vec<u8> = bincode::serialize(&target).unwrap();
-        proxy_wasm::hostcalls::set_shared_data("key", Some(&encoded), None);
+        proxy_wasm::hostcalls::set_shared_data(&self.key.clone(), Some(&encoded), None);
     }
     pub fn update(&mut self, time: i32) -> bool {
         if self.Min != time {
